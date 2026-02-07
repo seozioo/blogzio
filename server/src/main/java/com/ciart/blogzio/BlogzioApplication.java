583deff086a93d5d6a483f7fd1e310e438f5a937
SE <155702977+bulletproofvest@users.noreply.github.com>
2026-02-07 14:26:58 +0900

    Initial commit

---
+++ server/src/main/java/com/ciart/blogzio/BlogzioApplication.java
@@ -0,0 +1,13 @@
+package com.ciart.blogzio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogzioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogzioApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ciart/blogzio/BlogzioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciart.blogzio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogzioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
