65de2ff61abdb0bce049eddf6e521e48be4cce4b
Pdom <pdom0327@gmail.com>
2026-02-11 21:01:01 +0900

    fix: password encoder

--- server/src/main/java/com/ciart/blogzio/BlogzioApplication.java
+++ server/src/main/java/com/ciart/blogzio/BlogzioApplication.java
@@ -2,6 +2,9 @@ package com.ciart.blogzio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BlogzioApplication {
@@ -10,4 +13,8 @@ public class BlogzioApplication {
 		SpringApplication.run(BlogzioApplication.class, args);
 	}
 
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
